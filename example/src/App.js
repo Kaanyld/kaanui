30152f8207296c0d34ac5c81ca385bd41b1daa0e
Kaan <kaanyldrm98@gmail.com>
2023-04-16 06:17:24 +0300

    last commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,28 @@
 import React from 'react'
 
-import { ExampleComponent } from 'kaanui'
+import { KaanButton } from 'kaanui'
 import 'kaanui/dist/index.css'
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+const App = () => { 
+  return <>
+  <KaanButton text="Click" type="primary" />
+  <br /> <br />
+
+  <KaanButton type="default" text="Default" />
+  <br />
+  <br />
+
+  <KaanButton type="dashed" text="Dashed" />
+  <br />
+  <br />
+
+  <KaanButton type="text" text="Text" />
+  <br />
+  <br />
+
+  <KaanButton type="link" text="Link" />
+
+  </> 
 }
 
 export default App
